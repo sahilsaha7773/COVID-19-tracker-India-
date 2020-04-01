16689ae14098bb18e3086593a423c0f41d0370a8
Sahil Saha <35343652+sahilsaha7773@users.noreply.github.com>
2020-04-01 11:10:18 +0530

    Fixes bug

--- tracker/views.py
+++ tracker/views.py
@@ -26,7 +26,6 @@ def home(request):
 	for divs in mydivs:
 		count.append(extract_contents(divs.find_all('span')))
 		label.append(extract_contents(divs.find_all('div')))
-	passatair = int(count[0][0].replace(',',''))
 	# totcases = int(int(count[1][0].replace(',',''))+int(count[2][0].replace(',',''))+int(count[3][0].replace(',',''))+int(count[4][0].replace(',','')))
 	# active = int(count[1][0].replace(',',''))
 	# cured = int(count[2][0].replace(',',''))
